45f1f0a2b4925ada6912a1c17b53d3e5af144ca2
Ziaullah Khan <ziaullahkhan@Ziaullahs-MacBook-Pro.local>
2019-10-24 20:49:36 +0530

    initial draft

---
+++ OPAL.Presentation/Controllers/Search/Models/BasicSearchViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace OPAL.Presentation.Models
+{
+    public class BasicSearchViewModel
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string DocumentTitle { get; set; }
+        public string DocumentDescription { get; set; }
+        public DateTime CreateOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdateOn { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
